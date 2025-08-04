11e28c6c92ee6499852981c7f353564f59340b13
Sean Murphy <sean@iamseanmurphy.com>
2025-08-04 13:28:22 +0000

    16.7

--- gradiant/functions.php
+++ gradiant/functions.php
@@ -5,7 +5,7 @@ function gradiant_setup() {
 /**
  * Define Theme Version
  */
-define( 'GRADIANT_THEME_VERSION', '16.6' );
+define( 'GRADIANT_THEME_VERSION', '16.7' );
 
 // Root path/URI.
 define( 'GRADIANT_PARENT_DIR', get_template_directory() );
